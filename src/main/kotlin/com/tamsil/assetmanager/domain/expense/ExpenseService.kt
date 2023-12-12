cb12b87b6968fffff799462f99d93d1cdb6f9d6e
junihong <tamsil807@gmail.com>
2023-12-12 21:22:47 +0900

    asset-manager-kotlin ExpenseDto created

--- src/main/kotlin/com/tamsil/assetmanager/domain/expense/ExpenseService.kt
+++ src/main/kotlin/com/tamsil/assetmanager/domain/expense/ExpenseService.kt
@@ -5,6 +5,7 @@ import com.tamsil.assetmanager.domain.payment.PaymentRepository
 import com.tamsil.assetmanager.web.dto.*
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ExpenseService(
@@ -15,12 +16,12 @@ class ExpenseService(
 
     private val log = LoggerFactory.getLogger(ExpenseService::class.java)
 
+    @Transactional
     fun create(expenseRequestDto: ExpenseRequestDto): ExpenseResponseDto {
         val payment = paymentRepository.findById(expenseRequestDto.paymentId).orElseThrow()
         val expense = expenseRequestDto.toEntity()
         expense.setPayment(payment)
         val savedEntity = expenseRepository.save(expense)
-
         val category = categoryRepository.findById(savedEntity.categoryId).orElseThrow()
 
         return ExpenseResponseDto(
@@ -44,6 +45,33 @@ class ExpenseService(
                     payment.name,
                     payment.description
                 )
+        )
+    }
+
+    fun findById(id: Long): ExpenseResponseDto {
+        val expense = expenseRepository.findById(id).orElseThrow()
+        val category = categoryRepository.findById(expense.categoryId).orElseThrow()
+        return ExpenseResponseDto(
+            expense.id,
+            expense.name,
+            expense.place,
+            expense.cost,
+            expense.description,
+            expense.expenseDate,
+            expense.isInstallments,
+            CategoryResponseDto(
+                category.id,
+                category.categoryType,
+                category.categoryName,
+                category.detailName,
+                category.description
+            ),
+            PaymentResponseDto(
+                expense.payment!!.id,
+                expense.payment!!.paymentCode,
+                expense.name,
+                expense.description
+            )
         )
     }
 }
